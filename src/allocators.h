3986100364ec5b45c6d73e73ae994662671b561b
utopiacoin <40309430+utopiacoin@users.noreply.github.com>
2018-06-16 20:27:16 -0400

    Update allocators.h

--- src/allocators.h
+++ src/allocators.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef NEWCOIN_ALLOCATORS_H
-#define NEWCOIN_ALLOCATORS_H
+#ifndef UTOPIACOIN_ALLOCATORS_H
+#define UTOPIACOIN_ALLOCATORS_H
 
 #include <map>
 #include <string>
@@ -265,4 +265,4 @@ typedef std::basic_string<char, std::char_traits<char>, secure_allocator<char> >
 // Byte-vector that clears its contents before deletion.
 typedef std::vector<char, zero_after_free_allocator<char> > CSerializeData;
 
-#endif // NEWCOIN_ALLOCATORS_H
+#endif // UTOPIACOIN_ALLOCATORS_H
