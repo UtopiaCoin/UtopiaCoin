49ea68afba4b7ff3e2dc2c97c8960af5ff38e860
utopiacoin <40309430+utopiacoin@users.noreply.github.com>
2018-06-16 20:56:10 -0400

    Update db.h

--- src/db.h
+++ src/db.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef NEWCOIN_DB_H
-#define NEWCOIN_DB_H
+#ifndef UTOPIACOIN_DB_H
+#define UTOPIACOIN_DB_H
 
 #include "clientversion.h"
 #include "serialize.h"
@@ -309,4 +309,4 @@ public:
     bool static Rewrite(const std::string& strFile, const char* pszSkip = NULL);
 };
 
-#endif // NEWCOIN_DB_H
+#endif // UTOPIACOIN_DB_H
